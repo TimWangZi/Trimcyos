13dbefac3cbec0cf1e1ee21a204238191719ca97
TimWangZi <53970763+TimWangZi@users.noreply.github.com>
2020-02-15 22:19:24 +0800

    2020/2/15/22:19 t_copy
    Update t_copy

--- src/tool/t_copy.cpp
+++ src/tool/t_copy.cpp
@@ -10,23 +10,33 @@ using namespace std;
 char code, buffer;//²Ù×÷Âë
 int main(int argc, char* argv[]) {
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (strcmp("F", argv[1]) == 0 && argc > 2) {
+	/*if (strcmp("F", argv[1]) == 0 && argc > 2) {
 		SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_INTENSITY);
 		cout << "error:";
 		SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		cout << "Unsport\n";
-	}
-	else if (strcmp("f", argv[1]) == 0 && argc > 2) {
-		FILE *fp = fopen(argv[2], "wb+");
+	/*}
+	else if (strcmp("f", argv[1]) == 0 && argc > 2) {*/
+		FILE *fp = fopen(argv[1], "wb+");
 		if (fp == NULL) {
 			SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_INTENSITY);
 			cout << "error:";
 			SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 			cout << "Can't open the file\n";
 		}
-		for (int i = 3; i < argc; i++) {
+		for (int i = 2; i < argc; i++) {
 			FILE *fp2 = fopen(argv[i], "rb+");
-			cout << argv[2] << '\t' << "<-" << argv[i] << '\t';
+			if (fp2 == NULL) {
+				cout << argv[1] << '\t' << "<-" << argv[i] << '\t';
+				cout << "<";
+				SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_INTENSITY);
+				cout << "fail";
+				SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+				cout << ">\n";
+				cout << "eixit the process";
+				return 1;
+			}
+			cout << argv[1] << '\t' << "<-" << argv[i] << '\t';
 			while (true) {
 				fread(&buffer, sizeof(buffer), 1, fp2);
 				if (feof(fp2))
@@ -40,13 +50,13 @@ int main(int argc, char* argv[]) {
 			cout << ">\n";
 			fclose(fp2);
 		}
-	}
+	/*}
 	else {
 		SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_INTENSITY);
 		cout << "error:";
 		SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		cout << "Undefine Command\n";
 		return 1;
-	}
+	}*/
 	return 0;
 }
