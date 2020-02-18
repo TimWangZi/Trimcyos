0bd6203c769ae6e4264915ca20ef723d5ff0154d
TimWangZi <53970763+TimWangZi@users.noreply.github.com>
2020-02-18 14:12:45 +0800

    2020/2/18/14:12
    尚未测试,cfunc.asm

--- src/asm/cfunc.asm
+++ src/asm/cfunc.asm
@@ -29,5 +29,78 @@ global system_hlt
 
 ;函数memory_write
 ;	#1目标地址(32bit)
-;	#2数据(8bit)
-;
+;	#2数据(16bit)
+;	返回值：无
+;	作用：向目标地址写入2个字节的数据
+memory_write:
+	add esp,4
+	pop eax
+	pop ebx
+	mov WORD[ds:eax],bx
+	sub esp,12
+	ret
+;函数memory_read
+;	#1目标地址(32bit)
+;	返回值：目标地址指向的数据(16bit)
+;	作用：向目标地址写入2个字节的数据
+memory_read:
+	add esp,4
+	pop ebx
+	xor eax,eax
+	mov ax,[ds:ebx]
+	sub esp,8
+	ret
+;函数io_write_8
+;	#1目标io地址(16bit)
+;	#2写入目标的数据(8bit)
+;	返回值：无
+;	作用：向目标端口写入1个字节的数据
+io_write_8:
+	add esp,4
+	pop edx
+	pop eax
+	out dx,al
+	sub esp,12
+	ret
+;函数io_read_8
+;	#1目标io地址(16bit)
+;	返回值：从目标地址读取的数据(8bit)
+;	作用：从目标端口读入1个字节的数据
+io_read_8:
+	add esp,4
+	pop edx
+	xor eax,eax
+	in al,dx
+	sub esp,8
+	ret
+;函数io_write_16
+;	#1目标io地址(16bit)
+;	#2写入目标的数据(16bit)
+;	返回值：无
+;	作用：向目标端口写入2个字节的数据
+io_write_16:
+	add esp,4
+	pop edx
+	pop eax
+	out dx,ax
+	sub esp,12
+	ret
+;函数io_read_16
+;	#1目标io地址(16bit)
+;	返回值：从目标地址读取的数据(16bit)
+;	作用：从目标端口读入2个字节的数据
+io_read_16:
+	add esp,4
+	pop edx
+	xor eax,eax
+	in ax,dx
+	sub esp,8
+	ret
+;函数system_hlt
+;	#NONE
+;	返回值：无
+;	作用：系统停机直到中断发生
+system_hlt:
+	hlt
+	ret
+
